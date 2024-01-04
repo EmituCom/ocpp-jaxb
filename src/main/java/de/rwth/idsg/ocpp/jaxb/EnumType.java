208e8fb23bec15c6d84a3bbc4532073b6e3dc72a
João Cunha <skidrow.jc@gmail.com>
2024-01-04 15:24:12 +0000

    FEAT: Testing some enum types
    Signed-off-by: João Cunha <skidrow.jc@gmail.com>

---
+++ src/main/java/de/rwth/idsg/ocpp/jaxb/EnumType.java
@@ -0,0 +1,10 @@
+package de.rwth.idsg.ocpp.jaxb;
+
+/**
+ * To be implemented by all enums
+ *
+ * @author João Cunha <skidrow.jc@gmail.com>
+ * @since 04.12.2024
+ */
+public interface EnumType {
+}
