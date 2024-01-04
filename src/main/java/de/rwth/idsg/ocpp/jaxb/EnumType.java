3c760ca08fc491639f1074c7c9f6860325404134
João Cunha <skidrow.jc@gmail.com>
2024-01-04 15:35:56 +0000

    FEAT: test
    Signed-off-by: João Cunha <skidrow.jc@gmail.com>

--- src/main/java/de/rwth/idsg/ocpp/jaxb/EnumType.java
+++ src/main/java/de/rwth/idsg/ocpp/jaxb/EnumType.java
@@ -1,7 +1,7 @@
 package de.rwth.idsg.ocpp.jaxb;
 
 /**
- * To be implemented by all enums
+ * To be implemented by all enums for ocpp 2.0.1 and above
  *
  * @author João Cunha <skidrow.jc@gmail.com>
  * @since 04.12.2024
