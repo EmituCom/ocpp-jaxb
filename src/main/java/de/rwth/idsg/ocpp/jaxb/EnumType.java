07e920366e315557d10a938b3751d46fa0955ca5
João Cunha <Zyr00@users.noreply.github.com>
2024-01-08 12:20:14 +0000

    Enum tests (#1)
    * FEAT: Testing some enum types
    
    Signed-off-by: João Cunha <skidrow.jc@gmail.com>
    
    * FEAT: test
    
    Signed-off-by: João Cunha <skidrow.jc@gmail.com>
    
    * FEAT: added interfaces to enums
    
    Signed-off-by: João Cunha <skidrow.jc@gmail.com>
    
    * FEAT: added value function
    
    Signed-off-by: João Cunha <skidrow.jc@gmail.com>
    
    ---------
    
    Signed-off-by: João Cunha <skidrow.jc@gmail.com>

---
+++ src/main/java/de/rwth/idsg/ocpp/jaxb/EnumType.java
@@ -0,0 +1,11 @@
+package de.rwth.idsg.ocpp.jaxb;
+
+/**
+ * To be implemented by all enums for ocpp 2.0.1 and above
+ *
+ * @author João Cunha <skidrow.jc@gmail.com>
+ * @since 04.12.2024
+ */
+public interface EnumType {
+    String value();
+}
